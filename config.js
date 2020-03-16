d071118be59ec7840985fd9ea5a756dcd2b13503
Bhargey Mehta <bhargey.mehta@gmail.com>
2020-03-16 16:42:29 +0530

    Reports API

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    "av_key": "replace_with_alphavantage_key",
+    "atlas_user": "bhargey",
+    "atlas_pass": "SEN-G22-SMAP",
+    "serv_port": 5000,
+    "use_av": false,
+    "use_fmprep": false,
+    "use_atlas": true
+};
+
+module.exports = config;
--- server.js
+++ server.js
@@ -3,16 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
 const MongoClient = require('mongodb').MongoClient;
+const config = require('./config.js');
 
-// setting up the server
+// setting up the server 
 app = express();
-const KEY = fs.readFileSync('key.txt');
-console.log(`Key obtained: ${KEY}`);
-const BASE = "https://www.alphavantage.co/query";
-const PORT = process.env.PORT || 5000;
-const dburl = "mongodb+srv://bhargey:SEN-G22-SMAP@smap-pmi7t.mongodb.net/test?retryWrites=true&w=majority";
-const use_alphavantage = false;
-
 
 // static folder
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -26,9 +20,13 @@ app.get('/', (req,res) =>{
 app.get('/api/prices/:symbol', (req, res) => {
     symbol = req.params.symbol;
 
-    if(use_alphavantage)
+    if(config.use_av || !config.use_atlas)
     {
-        let url = BASE + `?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=full&apikey=${KEY}`
+        let url = `https://www.alphavantage.co/query?\
+            function=TIME_SERIES_DAILY\
+            &symbol=${symbol}\
+            &outputsize=full\
+            &apikey=${config.av_key}`;
         // fetch the data from the database but since
         // we don't have one right now we use alphavantage
         axios.get(url)
@@ -62,29 +60,36 @@ app.get('/api/prices/:symbol', (req, res) => {
         });
     }
     else
-    {
-        MongoClient.connect(dburl, {useUnifiedTopology: true}, (err, client) => {
-            if(err)
+    {   
+        let dburi = `mongodb+srv://`+
+        `${config.atlas_user}:${config.atlas_pass}`+
+        `@smap-pmi7t.mongodb.net/test?retryWrites=true&w=majority`;
+
+        MongoClient.connect(dburi, {useUnifiedTopology: true}, (err, client) => {
+            if(err){
                 console.log(err);
-            const collection = client.db("data").collection("prices");
+            }else{
+                const collection = client.db("data").collection("prices");
+                const timestamp = [];
+                const prices = [];
 
-            const timestamp = [];
-            const prices = [];
-
-            collection.find({symbol: symbol})
-            .forEach(doc => {
-                timestamp.push(doc.timestamp);
-                prices.push(doc.prices);
-            })
-            .then(() => {
-                res.json({status: "OK", timestamp, prices});
-            })
-            .then(() => {
-                client.close();
-            })
-            .catch(err => {
-                res.json({status: "DB_ERROR"});
-            });
+                collection.find({symbol})
+                .forEach(doc => {
+                    timestamp.push(doc.timestamp);
+                    prices.push(doc.prices);
+                })
+                .then(() => {
+                    if(timestamp.length > 0)
+                        res.json({status: "OK", timestamp, prices});
+                    else
+                        res.json({status: "DB_ERROR", msg: "No data"});
+                    client.close();
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.json({status: "DB_ERROR", msg: "Unexpected Error"});
+                });
+            }
         });
     }
 });
@@ -95,10 +100,16 @@ app.get('/api/indicators/SMA/:symbol-:period', (req, res) => {
     period = req.params.period;
     
     
-    if(use_alphavantage)
+    if(config.use_av || !config.use_atlas)
     {
-        let url = BASE + `?function=sma&symbol=${symbol}&interval=daily&time_period=${period}&series_type=open&apikey=${KEY}`;
-
+        let url = `https://www.alphavantage.co/query?\
+            function=sma\
+            &symbol=${symbol}\
+            &interval=daily\
+            &time_period=${period}\
+            &series_type=open\
+            &apikey=${config.av_key}`;
+        
         axios.get(url)
             .then(res_av => {
                 data = res_av.data;
@@ -122,40 +133,54 @@ app.get('/api/indicators/SMA/:symbol-:period', (req, res) => {
     }
     else
     {
-        MongoClient.connect(dburl, {useUnifiedTopology: true}, (err, client) => {
-            if(err)
+        let dburi = `mongodb+srv://`+
+        `${config.atlas_user}:${config.atlas_pass}`+
+        `@smap-pmi7t.mongodb.net/test?retryWrites=true&w=majority`;
+
+        MongoClient.connect(dburi, {useUnifiedTopology: true}, (err, client) => {
+            if(err){
                 console.log(err);
-            const collection = client.db("data").collection("indicators");
+            }else{
+                const collection = client.db("data").collection("indicators");
 
-            const timestamp = [];
-            const analysis_data = [];
+                const timestamp = [];
+                const analysis_data = [];
 
-            collection.find({symbol, indicator: 'sma-'+period})
-            .forEach(doc => {
-                timestamp.push(doc.timestamp);
-                analysis_data.push(doc.value);
-            })
-            .then(() => {
-                res.json({status: "OK", timestamp, analysis_data});
-            })
-            .then(() => {
-                client.close();
-            })
-            .catch(err => {
-                res.json({status: "DB_ERROR"});
-            });
+                collection.find({symbol, indicator: 'sma-'+period})
+                .forEach(doc => {
+                    timestamp.push(doc.timestamp);
+                    analysis_data.push(doc.value);
+                })
+                .then(() => {
+                    if(timestamp.length > 0)
+                        res.json({status: "OK", timestamp, analysis_data});
+                    else
+                        res.json({status: "DB_ERROR", msg: "No data"});
+                    client.close();
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.json({status: "DB_ERROR", msg: "Unexpected Error"});
+                });
+            }
         });
     }
 });
 
-//indicators ema
+// indicators ema
 app.get('/api/indicators/EMA/:symbol-:period', (req, res) => {
     const symbol = req.params.symbol;
     const period = req.params.period;
     
-    if(use_alphavantage)
+    if(config.use_av || !config.use_atlas)
     {
-        const url = BASE + `?function=ema&symbol=${symbol}&interval=daily&time_period=${period}&series_type=open&apikey=${KEY}`;
+        let url = `https://www.alphavantage.co/query?\
+            function=ema\
+            &symbol=${symbol}\
+            &interval=daily\
+            &time_period=${period}\
+            &series_type=open\
+            &apikey=${config.av_key}`;
 
         axios.get(url)
         .then(res_av => {
@@ -179,31 +204,211 @@ app.get('/api/indicators/EMA/:symbol-:period', (req, res) => {
     }
     else
     {
-        MongoClient.connect(dburl, {useUnifiedTopology: true}, (err, client) => {
-            if(err)
+        let dburi = `mongodb+srv://`+
+        `${config.atlas_user}:${config.atlas_pass}`+
+        `@smap-pmi7t.mongodb.net/test?retryWrites=true&w=majority`;
+
+        MongoClient.connect(dburi, {useUnifiedTopology: true}, (err, client) => {
+            if(err){
                 console.log(err);
-            const collection = client.db("data").collection("indicators");
+            }else{
+                const collection = client.db("data").collection("indicators");
 
-            const timestamp = [];
-            const analysis_data = [];
+                const timestamp = [];
+                const analysis_data = [];
 
-            collection.find({symbol, indicator: 'ema-'+period})
-            .forEach(doc => {
-                timestamp.push(doc.timestamp);
-                analysis_data.push(doc.value);
-            })
-            .then(() => {
-                res.json({status: "OK", timestamp, analysis_data});
-            })
-            .then(() => {
-                client.close();
-            })
-            .catch(err => {
-                res.json({status: "DB_ERROR"});
-            });
+                collection.find({symbol, indicator: 'ema-'+period})
+                .forEach(doc => {
+                    timestamp.push(doc.timestamp);
+                    analysis_data.push(doc.value);
+                })
+                .then(() => {
+                    if(timestamp.length > 0)
+                        res.json({status: "OK", timestamp, analysis_data});
+                    else
+                        res.json({status: "DB_ERROR", msg: "No data"});
+                    client.close();
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.json({status: "DB_ERROR", msg: "Unexpected Error"});
+                });
+            }
         });
     }
 });
 
+// annual reports
+app.get('/api/report/:symbol-:year', (req, res) => {
+    const symbol = req.params.symbol;
+    const year = parseInt(req.params.year);
 
+    if(config.use_fmprep || !config.use_atlas)
+    {
+        let url_income = 'https://financialmodelingprep.com/api/'+
+                'v3/financials/income-statement/'+
+                `${symbol}`;
+        let url_balance = 'https://financialmodelingprep.com/api/'+
+                'v3/financials/balance-sheet-statement/'+
+                `${symbol}`;
+        let url_cash = 'https://financialmodelingprep.com/api/'+
+                'v3/financials/cash-flow-statement/'+
+                `${symbol}`;
+        
+        let income_statement = axios.get(url_income)
+        .then(res_fmprep => {
+            let data = res_fmprep.data;
+            let result = {};
+            if(!data.financials){
+                throw 'BAD_API';
+            }else{
+                for(let i=0; i<data.financials.length; i++)
+                {
+                    rep_year = parseInt(data.financials[i].date.split('-')[0]);
+                    if(rep_year === year)
+                    {
+                        Object.keys(data.financials[i])
+                        .forEach(key => {
+                            if(key !== 'date')
+                                result[key] = parseFloat(data.financials[i][key]);
+                        });
+
+                        return result;
+                    }
+                }
+            }
+            return {};
+        })
+        .catch(err => {
+            throw err;
+        });
+
+        let balance_statement = axios.get(url_balance)
+        .then(res_fmprep => {
+            let data = res_fmprep.data;
+            let result = {};
+            if(!data.financials){
+                throw 'BAD_API';
+            }else{
+                for(let i=0; i<data.financials.length; i++)
+                {
+                    rep_year = parseInt(data.financials[i].date.split('-')[0]);
+                    if(rep_year === year)
+                    {
+                        Object.keys(data.financials[i])
+                        .forEach(key => {
+                            if(key !== 'date')
+                                result[key] = parseFloat(data.financials[i][key]);
+                        });
+                        
+                        return result;
+                    }
+                }
+            }
+            return {};
+        })
+        .catch(err => {
+            throw err;
+        });
+
+        let cash_statement = axios.get(url_cash)
+        .then(res_fmprep => {
+            let data = res_fmprep.data;
+            let result = {};
+            if(!data.financials){
+                throw 'BAD_API';
+            }else{
+                for(let i=0; i<data.financials.length; i++)
+                {
+                    rep_year = parseInt(data.financials[i].date.split('-')[0]);
+                    if(rep_year === year)
+                    {
+                        Object.keys(data.financials[i])
+                        .forEach(key => {
+                            if(key !== 'date')
+                                result[key] = parseFloat(data.financials[i][key]);
+                        });
+                        
+                        return result;
+                    }
+                }
+            }
+            return {};
+        })
+        .catch(err => {
+            throw err;
+        });
+
+        Promise.all([income_statement, balance_statement, cash_statement])
+        .then(values => {
+            let final = {status: "OK", symbol, year};
+            if(Object.keys(values[0]).length > 0)
+                final.income_statement = values[0];
+            else{
+                res.json({status: "API_ERROR", msg: "NO DATA"});
+                return;
+            }
+            
+            if(Object.keys(values[1]).length > 0)
+                final.balance_statement = values[1];
+            else{
+                res.json({status: "API_ERROR", msg: "NO DATA"});
+                return;
+            }
+            
+            if(Object.keys(values[2]).length > 0)
+                final.cash_statement = values[2];
+            else{
+                res.json({status: "API_ERROR", msg: "NO DATA"});
+                return;
+            }
+
+            res.json(final);
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({status: "API_ERROR", msg: "UNEXPECTED ERROR"});
+        });
+        
+    }
+    else
+    {
+        let dburi = `mongodb+srv://`+
+        `${config.atlas_user}:${config.atlas_pass}`+
+        `@smap-pmi7t.mongodb.net/test?retryWrites=true&w=majority`;
+
+        MongoClient.connect(dburi, {useUnifiedTopology: true}, (err, client) => {
+            if(err){
+                console.log(err);
+                res.json({status: "DB_ERROR", msg: "CAN'T CONNECT"});
+            }else{
+                const collection = client.db("data").collection("reports");
+
+                let reports = [];
+                collection.find({symbol, year})
+                .forEach(doc => reports.push(doc))
+                .then(() => client.close())
+                .then(() => {
+                    if(reports.length === 1)
+                    {
+                        delete reports[0]._id;
+                        reports[0].status = "OK";
+                        res.json(reports[0]);
+                    }
+                    else if(reports.length === 0)
+                        res.json({status: "DB_ERROR", msg: "NO DATA"});
+                    else
+                        res.json({status: "DB_ERROR", msg: "UNEXPECTED ERROR"});
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.json({status: "DB_ERROR", msg: "UNEXPECTED ERROR"});
+                });
+            }
+        });
+    }
+});
+
+// start listening
+const PORT = process.env.PORT || config.serv_port;
 app.listen(PORT, () => console.log(`Server running on ${PORT}...`));
