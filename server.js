b281ad2c204acf5a9f086aeb3bace4f8f1ec8bff
Bhargey Mehta <bhargey.mehta@gmail.com>
2020-03-17 17:09:25 +0530

    new user post method

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const config = require('./config');
+const bodyParser = require('body-parser');
 
 // setting up the server 
 app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
 // static folder
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -23,6 +25,11 @@ app.use('/api/indicators/EMA', require('./routes/api/indicators/ema'));
 // annual reports
 app.use('/api/report', require('./routes/api/report'));
 
+// add user
+const isValidUser = require('./routes/auth/uservalidate');
+app.use('/useradd', require('./routes/auth/useradd'));
+
 // start listening
 const PORT = process.env.PORT || config.serv_port;
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}...`));
