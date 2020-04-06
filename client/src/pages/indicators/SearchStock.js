4021e68b54ba338534436b537d92dea4d4d6fe20
Bhargey Mehta <bhargey.mehta@gmail.com>
2020-04-06 20:34:34 +0530

    chart drawer fix

--- client/src/pages/indicators/SearchStock.js
+++ client/src/pages/indicators/SearchStock.js
@@ -46,8 +46,7 @@ class SearchStock extends React.Component{
                     dataPoints: []
                 }
                 let api_res = data2['timestamp'].map((ele, index) => {
-                    let date_nums = ele.split('-');
-                    return [new Date(date_nums[0], date_nums[1], date_nums[2]), data2['analysis_data'][index]];
+                    return [new Date(ele), data2['analysis_data'][index]];
                 });
     
                 api_res.sort((a, b) => a[0]-b[0]);
@@ -164,8 +163,7 @@ class SearchStock extends React.Component{
                     options.data[0].name = "price";
 
                     let api_res = data['timestamp'].map((ele, index) => {
-                        let date_nums = ele.split('-');
-                        return [new Date(date_nums[0], date_nums[1], date_nums[2]), data['prices'][index]];
+                        return [new Date(ele), data['prices'][index]];
                     });
 
                     api_res.sort((a, b) => a[0]-b[0]);
